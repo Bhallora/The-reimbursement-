06717219ecf507fa0f694f9f598dc9826b12f07c
Pallab Borah <pallab.borah@kdi.kongsberg.com>
2019-08-29 19:42:06 +0530

    add corrected file names

---
+++ src/components/side-drawer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './side-drawer.css';
+import { NavLink, HashRouter } from 'react-router-dom';
+//import { Provider}from 'react-redux';
+
+
+const sideDrawer = props => {
+    let drawerClasses = 'side-drawer';
+    if (props.show) {
+        drawerClasses = 'side-drawer open';
+    }
+    return (
+        <HashRouter>
+            <nav className={drawerClasses}>
+
+
+                <ul>
+                    <li>
+                        <NavLink exact to="/"> Home </NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="/components/view-Applications">View Applications</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/components/new-Entry"> New  Entry </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/"> Log out </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </HashRouter>
+
+    );
+};
+export default sideDrawer;
